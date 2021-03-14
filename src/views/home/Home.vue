dea48e9cdbddd03fa647a4081c67ce4643b77227
DamonMok <damonmok@Damon-2.local>
2021-03-14 17:59:12 +0800

    首页滚动到顶部功能

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,127 +5,28 @@
       <template #center>购物街</template>
     </nav-bar>
 
-    <!-- 轮播图 -->
-    <home-swiper :banners="banners"></home-swiper>
+    <scroll ref="scroll" class="scroll" :pullup="true" @scrollToEnd="loadMore">
+      <!-- 轮播图 -->
+      <home-swiper :banners="banners"></home-swiper>
 
-    <!-- 4个推荐 -->
-    <home-recommend :recommends="recommend"></home-recommend>
+      <!-- 4个推荐 -->
+      <home-recommend :recommends="recommend"></home-recommend>
 
-    <!-- 本周流行 -->
-    <home-feature></home-feature>
+      <!-- 本周流行 -->
+      <home-feature></home-feature>
 
-    <!-- 流行、新款、精选 tab-control -->
-    <tab-control
-      :titles="['流行', '新款', '精选']"
-      class="tab-control"
-      @tabClick="tabClick">
-    </tab-control>
+      <!-- 流行、新款、精选 tab-control -->
+      <tab-control
+        :titles="['流行', '新款', '精选']"
+        class="tab-control"
+        @tabClick="tabClick">
+      </tab-control>
 
-    <!-- 商品列表 -->
-    <goods-list :goods="showGoods"></goods-list>
+      <!-- 商品列表 -->
+      <goods-list :goods="showGoods"></goods-list>
+    </scroll>
 
-    <ul>
-      <li>item1</li>
-      <li>item2</li>
-      <li>item3</li>
-      <li>item4</li>
-      <li>item5</li>
-      <li>item6</li>
-      <li>item7</li>
-      <li>item8</li>
-      <li>item9</li>
-      <li>item10</li>
-      <li>item11</li>
-      <li>item12</li>
-      <li>item13</li>
-      <li>item14</li>
-      <li>item15</li>
-      <li>item16</li>
-      <li>item17</li>
-      <li>item18</li>
-      <li>item19</li>
-      <li>item20</li>
-      <li>item21</li>
-      <li>item22</li>
-      <li>item23</li>
-      <li>item24</li>
-      <li>item25</li>
-      <li>item26</li>
-      <li>item27</li>
-      <li>item28</li>
-      <li>item29</li>
-      <li>item30</li>
-      <li>item31</li>
-      <li>item32</li>
-      <li>item33</li>
-      <li>item34</li>
-      <li>item35</li>
-      <li>item36</li>
-      <li>item37</li>
-      <li>item38</li>
-      <li>item39</li>
-      <li>item40</li>
-      <li>item41</li>
-      <li>item42</li>
-      <li>item43</li>
-      <li>item44</li>
-      <li>item45</li>
-      <li>item46</li>
-      <li>item47</li>
-      <li>item48</li>
-      <li>item49</li>
-      <li>item50</li>
-      <li>item51</li>
-      <li>item52</li>
-      <li>item53</li>
-      <li>item54</li>
-      <li>item55</li>
-      <li>item56</li>
-      <li>item57</li>
-      <li>item58</li>
-      <li>item59</li>
-      <li>item60</li>
-      <li>item61</li>
-      <li>item62</li>
-      <li>item63</li>
-      <li>item64</li>
-      <li>item65</li>
-      <li>item66</li>
-      <li>item67</li>
-      <li>item68</li>
-      <li>item69</li>
-      <li>item70</li>
-      <li>item71</li>
-      <li>item72</li>
-      <li>item73</li>
-      <li>item74</li>
-      <li>item75</li>
-      <li>item76</li>
-      <li>item77</li>
-      <li>item78</li>
-      <li>item79</li>
-      <li>item80</li>
-      <li>item81</li>
-      <li>item82</li>
-      <li>item83</li>
-      <li>item84</li>
-      <li>item85</li>
-      <li>item86</li>
-      <li>item87</li>
-      <li>item88</li>
-      <li>item89</li>
-      <li>item90</li>
-      <li>item91</li>
-      <li>item92</li>
-      <li>item93</li>
-      <li>item94</li>
-      <li>item95</li>
-      <li>item96</li>
-      <li>item97</li>
-      <li>item98</li>
-      <li>item99</li>
-      <li>item100</li>
-    </ul>
+    <back-top @click.native="backTop"></back-top>
   </div>
 </template>
 
@@ -135,8 +36,10 @@ import HomeRecommend from "./childComps/HomeRecommend";
 import HomeFeature from "./childComps/HomeFeature";
 
 import NavBar from "components/common/navbar/NavBar";
+import Scroll from "components/common/scroll/Scroll"
 import TabControl from "components/content/tabControl/TabControl";
 import GoodsList from "components/content/goods/GoodsList";
+import BackTop from "components/content/backTop/BackTop"
 
 import { getHomeDatas, getHomeGoods } from "network/home";
 
@@ -159,8 +62,10 @@ export default {
     HomeRecommend,
     HomeFeature,
     NavBar,
+    Scroll,
     TabControl,
-    GoodsList
+    GoodsList,
+    BackTop
   },
   created() {
     // 获取首页数据
@@ -189,7 +94,6 @@ export default {
       console.log(this.currentType);
     },
 
-
     getHomeDatas() {
       getHomeDatas().then((res) => {
         this.banners = res.data.data.banner.list
@@ -200,11 +104,18 @@ export default {
     getHomeGoods(type) {
       const page = this.goods[type].page + 1;
       getHomeGoods(type, page).then((res) => {
-        console.log(res);
         this.goods[type].list.push(...res.data.data.list); // 使用js的数组解构
         this.goods[type].page += 1;
       });
     },
+
+    backTop() {
+      this.$refs.scroll.scrollTo(0, 0, 500)
+    },
+
+    loadMore() {
+      console.log('loadMore...');
+    }
   },
   computed: {
     showGoods() {
@@ -217,6 +128,8 @@ export default {
 <style scoped>
 #home {
   padding-top: 44px;
+  height: 100vh;
+  position: relative;
 }
 
 .home-nav {
@@ -233,5 +146,14 @@ export default {
 .tab-control {
   position: sticky;
   top: 44px;
+}
+
+.scroll {
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+  overflow: hidden ;
 }
 </style>
