0acf95980579b38b11f9f4f2717df50122e82e20
DamonMok <damonmok@Damon-2.local>
2021-03-16 18:29:43 +0800

    完成详情页导航栏

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,14 +1,21 @@
 <template>
-  <div id="detail">{{ id }}</div>
+  <div id="detail">
+    <detail-nav-bar></detail-nav-bar>
+  </div>
 </template>
 
 <script>
+import DetailNavBar from 'views/detail/childComps/DetailNavBar'
+
 export default {
   name: 'detail',
   data() {
     return {
       id: null
     };
+  },
+  components: {
+    DetailNavBar
   },
   created() {
     this.id = this.$route.params.id
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,57 @@
+<template>
+  <nav-bar>
+    <template #left>
+      <img class="back" src="~assets/img/common/back.svg" alt="" @click="didClickedBack">
+    </template>
+    <template #center>
+      <div class="title">
+        <div class="title-item" v-for="(item, index) in title" :key=index :class="{active: currentIndex===index}" @click="didClickedNavBar(index)">{{ item }}</div>
+      </div>
+    </template>
+  </nav-bar>
+</template>
+
+<script>
+import NavBar from "components/common/navbar/NavBar";
+
+export default {
+  components: {
+    NavBar,
+  },
+  data() {
+    return {
+      title:['商品', '参数', '评论', '推荐'],
+      currentIndex:0
+    }
+  },
+  methods: {
+    didClickedNavBar(index) {
+      this.currentIndex = index
+    },
+
+    didClickedBack() {
+      this.$router.go(-1)
+    }
+  }
+};
+</script>
+
+<style scoped>
+.title {
+  display: flex;
+  font-size: 13px;
+}
+
+.title-item {
+  flex: 1;
+}
+
+.active {
+  color: var(--color-high-text);
+}
+
+.back {
+  margin-top: 10px;
+}
+
+</style>
